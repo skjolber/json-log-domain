df3ff3bc84b50f1d8f6bb4d5675a21c691fbcabc
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2017-07-21 00:54:15 +0200

    add compilation to unit testing

--- codegen/src/test/java/yaml/logger/YamlTest.java
+++ codegen/src/test/java/yaml/logger/YamlTest.java
@@ -1,6 +1,18 @@
 package yaml.logger;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 
 import org.junit.Test;
 
@@ -15,6 +27,27 @@ public class YamlTest {
 		JavaGenerator.generate(new File("src/test/resources/yaml/agresso.yaml"), directory);
 		JavaGenerator.generate(new File("src/test/resources/yaml/global.yaml"), directory);
 		JavaGenerator.generate(new File("src/test/resources/yaml/network.yaml"), directory);
+		
+		// verify that the generated classes can be loaded
+		for(String domain : new String[]{"agresso", "global", "network"}) {
+			File network = new File(String.format("target/generated-sources/log/com/example/%s/", domain));
+			File[] results = network.listFiles(new FileFilter() {
+				public boolean accept(File pathname) {
+					return pathname.getName().endsWith(".java");
+				}
+			});
+
+			List<String> files = new ArrayList<>();
+			for(File result : results) {
+				files.add(result.getAbsolutePath());
+			}
+
+		    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		    int status = compiler.run(null, null, null, files.toArray(new String[files.size()]));
+		    if(status != 0) {
+		    	throw new IllegalArgumentException();
+		    }			
+		}
 	}
 	
 /*
