54820dacf5ac38fe85ac8349accaaf2b0fdff224
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2018-01-08 00:29:10 +0100

    improve gradle task

--- gradle-plugin/src/main/java/com/github/skjolberg/log/domain/codegen/gradle/JsonLogDomainTask.java
+++ gradle-plugin/src/main/java/com/github/skjolberg/log/domain/codegen/gradle/JsonLogDomainTask.java
@@ -16,7 +16,10 @@ import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskAction;
 
 import com.github.skjolber.log.domain.codegen.DomainFactory;
+import com.github.skjolber.log.domain.codegen.ElasticGenerator;
+import com.github.skjolber.log.domain.codegen.MarkdownGenerator;
 import com.github.skjolber.log.domain.codegen.logstash.LogbackGenerator;
+import com.github.skjolber.log.domain.codegen.stackdriver.StackDriverGenerator;
 import com.github.skjolberg.log.domain.codegen.gradle.ext.Elastic;
 import com.github.skjolberg.log.domain.codegen.gradle.ext.Logback;
 import com.github.skjolberg.log.domain.codegen.gradle.ext.Markdown;
@@ -24,7 +27,8 @@ import com.github.skjolberg.log.domain.codegen.gradle.ext.StackDriver;
 
 public class JsonLogDomainTask extends DefaultTask {
 
-	public static final String DEFAULT_DESTINATION_DIR = "/generatedsources/src/main/java";
+	public static final String DEFAULT_DESTINATION_DIR = "/generatedSources/src/main/java";
+	public static final String DEFAULT_DESTINATION_RESOURCE_DIR = "/generatedSources/src/main/resources";
 			
 	protected ConfigurableFileCollection definitions;
 	
@@ -39,24 +43,67 @@ public class JsonLogDomainTask extends DefaultTask {
     	
     	Set<File> files = definitions.getFiles();
     	for(File file : files) {
+    		com.github.skjolber.log.domain.model.Domain result = DomainFactory.parse(Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8));
+
         	if(logback != null) {
-        		
         		File destination = logback.getOutputDirectory().getOrElse(new File(getProject().getBuildDir() + DEFAULT_DESTINATION_DIR));
         		
         		Path javaOutput = destination.toPath();
 		    	
 		    	if(!Files.exists(javaOutput)) Files.createDirectories(javaOutput);
 
-        		com.github.skjolber.log.domain.model.Domain result = DomainFactory.parse(Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8));
+    			System.out.println("Generating Logback Java output to " + javaOutput.toAbsolutePath());
 
     			new LogbackGenerator().generate(result, javaOutput);
     			
-    			System.out.println("Generating Logback Java output to " + javaOutput.toAbsolutePath());
+    			SourceSetContainer sourceSets = (SourceSetContainer) getProject().getProperties().get("sourceSets");
+
+    			sourceSets.getByName("main").getJava().getSrcDirs().add(javaOutput.toFile());
+        	}
+        	
+        	if(stackDriver != null) {
+        		File destination = stackDriver.getOutputDirectory().getOrElse(new File(getProject().getBuildDir() + DEFAULT_DESTINATION_DIR));
+        		
+        		Path javaOutput = destination.toPath();
+		    	
+		    	if(!Files.exists(javaOutput)) Files.createDirectories(javaOutput);
+
+    			System.out.println("Generating Stackdriver Java output to " + javaOutput.toAbsolutePath());
+
+    			new StackDriverGenerator().generate(result, javaOutput);
 
     			SourceSetContainer sourceSets = (SourceSetContainer) getProject().getProperties().get("sourceSets");
 
     			sourceSets.getByName("main").getJava().getSrcDirs().add(javaOutput.toFile());
         	}
+
+        	if(markdown != null) {
+        		File destination = markdown.getOutputDirectory().getOrElse(new File(getProject().getBuildDir() + DEFAULT_DESTINATION_RESOURCE_DIR));
+        		
+        		Path output = destination.toPath().resolve(result.getName() + ".md");
+		    	
+		    	if(!Files.exists(output.getParent())) Files.createDirectories(output.getParent());
+
+		    	boolean logbackCodeGenerated = markdown.getLogback().getOrElse(logback != null);
+		    	boolean stackDriverCodeGenerated = markdown.getStackDriver().getOrElse(stackDriver != null);
+		    	
+    			MarkdownGenerator.generate(result, output, logbackCodeGenerated, stackDriverCodeGenerated);
+    			
+    			System.out.println("Wrote Markdown output to " + output.toAbsolutePath() + " for " + result.getName());
+        	}
+
+        	if(elastic != null) {
+        		File destination = elastic.getOutputDirectory().getOrElse(new File(getProject().getBuildDir() + DEFAULT_DESTINATION_RESOURCE_DIR));
+        		
+        		Path output = destination.toPath().resolve(result.getName() + ".mapping.json");
+		    	
+		    	if(!Files.exists(output.getParent())) Files.createDirectories(output.getParent());
+
+    			ElasticGenerator.generate(result, output);
+    			
+    			System.out.println("Wrote Elastic output to " + output.toAbsolutePath() + " for " + result.getName());
+        	}
+
     	}
     }
 
